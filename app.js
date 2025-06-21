afc2718f20d629b730ed9f4688395241273c1510
bazil <zamanbazil123@gmail.com>
2025-06-21 15:20:50 +0530

    update

---
+++ app.js
@@ -0,0 +1,24 @@
+const http= require('http');
+const port = 4000;
+
+const server= http.createServer(function (req , res ) {
+    if(req.url==='/'){
+        res.write('man')
+    }else if(req.url==='/contact'){
+        res.write('contact')
+    }res.end();
+    
+});
+
+
+
+server.listen(port, function (error) {
+    if (error) {
+        console.log('something went wrong', error);
+
+    } else {
+        console.log('good' + port);
+
+    }
+
+});
