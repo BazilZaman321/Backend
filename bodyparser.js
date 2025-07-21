b77d51658899e96897030873ac5b70d03b649d3c
bazil <zamanbazil123@gmail.com>
2025-07-21 09:56:00 +0530

    update

---
+++ bodyparser.js
@@ -0,0 +1,18 @@
+const express= require('express');
+const app= express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.post('/submit',(req,res)=>{
+    console.log(req.body);
+  res.send(`given : ${JSON.stringify(req.body)}`); 
+    
+})
+
+
+app.listen(3000,()=>{
+    console.log("good");
+    
+})
